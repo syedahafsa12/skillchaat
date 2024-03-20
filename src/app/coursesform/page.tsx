70db6106bb73cd926863771fedb9b220bc7153f6
Syeda Hafsa <syedahafsa772@gmail.com>
2024-03-20 17:17:40 +0500

    mobile responsive done finishlast2

--- src/app/coursesform/page.tsx
+++ src/app/coursesform/page.tsx
@@ -84,7 +84,7 @@ function MusicSchoolContactUs() {
       {/* BackgroundBeams and Content remain unchanged */}
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
       <div className="max-w-2xl mx-auto p-4 relative z-10">
-        <h1 className="text-lg md:text-7xl sm:text-10xl text-center font-sans font-bold mb-8 text-orange">
+        <h1 className="h1-contact text-lg md:text-7xl sm:text-10xl text-center font-sans font-bold mb-8 text-orange">
           Contact Us
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
