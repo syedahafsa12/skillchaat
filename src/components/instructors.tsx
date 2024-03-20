6ab4e3b2ed87635e02f22e83990a2935ef2f22fa
Syeda Hafsa <syedahafsa772@gmail.com>
2024-03-20 16:03:19 +0500

    mobile responsive done

--- src/components/instructors.tsx
+++ src/components/instructors.tsx
@@ -40,7 +40,7 @@ function Instructors() {
     <div className="instructors relative h-[40rem] overflow-hidden flex items-center justify-center">
     <div className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
         <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">Meet Our Instructors</h2>
-        <p className="ins-p text-base md:text-lg text-white text-center mb-4">Discover the talented professionals who will guide your Web Development Journey</p>
+        <p className="ins-p text-base md:text-lg text-white text-center mb-4">Discover the talented professionals who will guide your Journey</p>
         <div className="flex flex-row items-center justify-center mb-10 w-full">
             <AnimatedTooltip items={instructors} />
         </div>
